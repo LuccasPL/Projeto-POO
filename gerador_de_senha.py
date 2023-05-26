db82c817ee970c4d2afa3f364c249b93f238bdcb
LuccasPL <117682893+LuccasPL@users.noreply.github.com>
2023-05-26 11:59:36 +0100

    Delete gerador_de_senha.py

--- gerador_de_senha.py
+++
@@ -1,8 +0,0 @@
-import string as s
-from random import *
-
-ch = s.ascii_letters + s.digits + s.punctuation
-
-senha = ''.join(choice(ch)for j in range(randint(8,16)))
-
-print('Senha gerada é: ' + senha)
